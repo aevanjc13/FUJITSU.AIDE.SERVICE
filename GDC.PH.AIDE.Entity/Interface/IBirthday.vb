920b0c6856640210c888f6ed250c828f870221b8
aevan batongbacal <aevan_jc13@yahoo.com>
2018-06-24 20:05:46 +0800

    1 uploa

---
+++ GDC.PH.AIDE.Entity/Interface/IBirthday.vb
@@ -0,0 +1,11 @@
+﻿Imports GDC.PH.AIDE.BusinessLayer
+Public Interface IBirthday
+    Property EMP_ID As Integer
+    Property FIRST_NAME As String
+    Property LAST_NAME As String
+    Property BIRTHDAY As Date
+    Property IMAGE_PATH As String
+
+    Function GetAllBirthday(ByVal email As String) As List(Of BirthdaySet)
+    Function GetBirthdayByMonth(ByVal email As String) As List(Of BirthdaySet)
+End Interface
---
+++ GDC.PH.AIDE.Entity/Interface/IResourcePlanner.vb
@@ -0,0 +1,26 @@
+﻿Imports GDC.PH.AIDE.BusinessLayer
+''' <summary>
+''' By Jhunell G. Barcenas
+''' </summary>
+''' <remarks></remarks>
+Public Interface IResourcePlanner
+
+    Property EmployeeID As Integer
+    Property Name As String
+    Property dateFrom As Date
+    Property dateTo As Date
+    Property Status As Integer
+    Property Description As String
+    Property Image_Path As String
+    Property dateEntry As Date
+
+    Function InsertResourcePlanner(ByVal resource As ResourcePlannerSet) As Boolean
+    Function UpdateResourcePlanner(ByVal resource As ResourcePlannerSet) As Boolean
+    Function ViewEmpResourcePlanner(ByVal email As String) As List(Of ResourcePlannerSet)
+    Function GetStatusResourcePlanner() As List(Of ResourcePlannerSet)
+    Function GetResourcePlannerByEmpID(ByVal empID As Integer, ByVal deptID As Integer, ByVal month As Integer, ByVal year As Integer) As List(Of ResourcePlannerSet)
+    Function GetAllEmpResourcePlanner(ByVal email As String, ByVal month As Integer, ByVal year As Integer) As List(Of ResourcePlannerSet)
+    Function GetAllEmpResourcePlannerByStatus(ByVal email As String, ByVal month As Integer, ByVal year As Integer, ByVal status As Integer) As List(Of ResourcePlannerSet)
+    Function GetAllStatusResourcePlanner() As List(Of ResourcePlannerSet)
+    Function GetResourcePlanner(ByVal email As String, ByVal status As Integer, ByVal toBeDisplayed As Integer) As List(Of ResourcePlannerSet)
+End Interface
